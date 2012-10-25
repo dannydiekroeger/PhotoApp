4f8bb395e46ff68393a1659634bdd57a060eee41
Danny Diekroeger <dannydiekroeger@gmail.com>
2012-10-25 11:10:49 -0700

    Flow is complete except for receiving invites

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
 		<p><a href="#popup" data-role="button" data-icon="plus" data-iconpos="bottom" button-color="red">Create an Event</a></p>
 		<br/><br/><br/><br/>
-		<p><a href="#two" data-role="button">My Events</a></p>
+		<p><a href="#myevents" data-role="button">My Events</a></p>
 		<p><a href="#settings" data-role="button">Settings</a></p>		
 <!--		<p><a href="#popup" data-role="button" data-rel="dialog" data-transition="pop">Show page "popup" (as a dialog)</a></p>
 	</div><!-- /content --> 
@@ -42,42 +42,97 @@
 	
 </div><!-- /page one -->
 
-
 <!-- Start of second page: #two -->
-<div data-role="page" id="two" data-add-back-btn="true">
+<div data-role="page" id="myevents" data-add-back-btn="true">
 
 	<div data-role="header">
-		<h1>Two</h1>
+		<h1>My Events</h1>
 	</div><!-- /header -->
 
 	<div data-role="content">	
-		<h2>Two</h2>
-		<p>I have an id of "two" on my page container. I'm the second page container in this multi-page template.</p>	
-		<p>Notice that the theme is different for this page because we've added a few <code>data-theme</code> swatch assigments here to show off how flexible it is. You can add any content or widget to these pages, but we're keeping these simple.</p>	
-		<p><a href="#one" data-direction="reverse" data-role="button" data-theme="b">Back to page "one"</a></p>	
+		<h2>My Events</h2>
+		<p>Here will be a list of all your events</p>
 		
 	</div><!-- /content -->
 
 </div>
 </div><!-- /page two -->
 
+<!-- Settings page-->
+<div data-role="page" id="settings" data-add-back-btn="true">
+
+	<div data-role="header">
+		<h1>Settings</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<h2>Settings</h2>
+		<p>Edit your settings here</p>	
+
+		
+	</div><!-- /content -->
+
+</div>
+</div><!-- end Settings page-->
+
+<!-- Event page-->
+<div data-role="page" id="event">
+
+	<div data-role="header">
+		<h1>EventName</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<h2>Sample Event</h2>
+		<p>This is a sample event</p>	
+		
+		<a data-role="button" data-theme="b" data-icon="star" data-pos="right">Take Picture</a>	
+		
+		<a href="#eventgallery" data-role="button" data-theme="b">Event Gallery</a>	
+		
+		<p><a href="#one" data-direction="reverse" data-role="button">Leave Event</a></p>	
+		
+	</div><!-- /content -->
+
+</div>
+</div><!-- end Event page-->
+
+
+<!-- Start of Event Gallery page-->
+<div data-role="page" id="eventgallery" data-add-back-btn="true">
+
+	<div data-role="header">
+		<h1>Event Gallery</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<h2>Event Gallery</h2>
+		<p>Here are all the pictures you have taken for this event!</p>	
+		
+	</div><!-- /content -->
+
+</div>
+</div><!-- end Even Gallery-->
+
 
 <!-- Start of third page: #popup -->
 <div data-role="page" id="popup">
 
 	<div data-role="header" data-theme="e">
-		<h1>Dialog</h1>
+		<h1>Create an Event</h1>
 	</div><!-- /header -->
 
 	<div data-role="content" data-theme="d">	
 		<h2>Create an Event</h2>
-		<form action="enter.php" method="post">
+		<!--Will need to change action to process data!! with method = post-->
+		<form action="#event">
 			<label for="foo">Event Name:</label>
 			<input type="text" name="username" id="foo">
 			<label for="bar">Add Friend:</label>
 			<input type="text" name="friend" id="bar">
-	        <input type="submit" value="Create Event">
-			</form>			
+	        <!--<input type="submit" data-theme="b" value="Create Event"> -->
+			</form>	
+		<a href="#event" data-role="button" data-theme="b">Create Event</a>	
 		<p><a href="#one" data-role="button" data-icon="minus">Cancel</a></p>	
 	</div><!-- /content -->
 
